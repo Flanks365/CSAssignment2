52c09be7a7899886cc7b405a0e225e37e1ec6f76
yuyue-creater <yyu129@my.bcit.ca>
2024-10-06 21:30:53 -0700

    More updates added

--- UploadServer/UploadServerThread.java
+++ UploadServer/UploadServerThread.java
@@ -1,23 +1,74 @@
+// import java.net.*;
+// import java.io.*;
+// import java.time.Clock;
+// public class UploadServerThread extends Thread {
+//    private Socket socket = null;
+//    public UploadServerThread(Socket socket) {
+//       super("DirServerThread");
+//       this.socket = socket;
+//    }
+//    public void run() {
+//       try {
+//          InputStream in = socket.getInputStream(); 
+//          HttpServletRequest req = new HttpServletRequest(in);  
+//          OutputStream baos = new ByteArrayOutputStream(); 
+//          HttpServletResponse res = new HttpServletResponse(baos); 
+//          HttpServlet httpServlet = new UploadServlet();
+//          httpServlet.doPost(req, res);
+//          OutputStream out = socket.getOutputStream(); 
+//          out.write(((ByteArrayOutputStream) baos).toByteArray());
+//          socket.close();
+//       } catch (Exception e) { e.printStackTrace(); }
+//    }
+// }
+
 import java.net.*;
 import java.io.*;
-import java.time.Clock;
+
 public class UploadServerThread extends Thread {
-   private Socket socket = null;
-   public UploadServerThread(Socket socket) {
-      super("DirServerThread");
-      this.socket = socket;
-   }
-   public void run() {
-      try {
-         InputStream in = socket.getInputStream(); 
-         HttpServletRequest req = new HttpServletRequest(in);  
-         OutputStream baos = new ByteArrayOutputStream(); 
-         HttpServletResponse res = new HttpServletResponse(baos); 
-         HttpServlet httpServlet = new UploadServlet();
-         httpServlet.doPost(req, res);
-         OutputStream out = socket.getOutputStream(); 
-         out.write(((ByteArrayOutputStream) baos).toByteArray());
-         socket.close();
-      } catch (Exception e) { e.printStackTrace(); }
-   }
+    private Socket socket = null;
+
+    public UploadServerThread(Socket socket) {
+        super("DirServerThread");
+        this.socket = socket;
+    }
+
+    public void run() {
+        try {
+            // Use BufferedInputStream to read the request
+            BufferedInputStream in = new BufferedInputStream(socket.getInputStream());
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+            
+            // Read the first line of the request
+            String requestLine = reader.readLine();
+            if (requestLine != null) {
+                String[] requestParts = requestLine.split(" ");
+                String method = requestParts[0];
+                String uri = requestParts[1];
+
+                HttpServletRequest req = new HttpServletRequest(in);
+                OutputStream baos = new ByteArrayOutputStream();
+                HttpServletResponse res = new HttpServletResponse(baos);
+                HttpServlet httpServlet = new UploadServlet();
+
+                // Check the HTTP method
+                if ("GET".equalsIgnoreCase(method) && "/".equals(uri)) {
+                    httpServlet.doGet(req, res);
+                } else if ("POST".equalsIgnoreCase(method)) {
+                    httpServlet.doPost(req, res);
+                } else {
+                    // Handle unsupported methods
+                    res.getOutputStream().write("HTTP/1.1 405 Method Not Allowed\r\n\r\n".getBytes());
+                }
+
+                // Send the response back to the client
+                OutputStream out = socket.getOutputStream();
+                out.write(((ByteArrayOutputStream) baos).toByteArray());
+                out.flush();
+                socket.close();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
--- UploadServer/UploadServlet.java
+++ UploadServer/UploadServlet.java
@@ -1,30 +1,92 @@
+// import java.io.*;
+// import java.time.Clock;
+// public class UploadServlet extends HttpServlet {
+//    protected void doPost(HttpServletRequest request, HttpServletResponse response) {
+//       try {
+//          InputStream in = request.getInputStream();   
+//          ByteArrayOutputStream baos = new ByteArrayOutputStream();  
+//          byte[] content = new byte[1];
+//          int bytesRead = -1;      
+//          while( ( bytesRead = in.read( content ) ) != -1 ) {  
+//             baos.write( content, 0, bytesRead );  
+//          }
+//          Clock clock = Clock.systemDefaultZone();
+//          long milliSeconds=clock.millis();
+//          OutputStream outputStream = new FileOutputStream(new File(String.valueOf(milliSeconds) + ".png"));
+//          baos.writeTo(outputStream);
+//          outputStream.close();
+//          PrintWriter out = new PrintWriter(response.getOutputStream(), true);
+//          File dir = new File(".");
+//          String[] chld = dir.list();
+//       	 for(int i = 0; i < chld.length; i++){
+//             String fileName = chld[i];
+//             out.println(fileName+"\n");
+//             System.out.println(fileName);
+//          }
+//       } catch(Exception ex) {
+//          System.err.println(ex);
+//       }
+//    }
+// }
 import java.io.*;
 import java.time.Clock;
+
 public class UploadServlet extends HttpServlet {
-   protected void doPost(HttpServletRequest request, HttpServletResponse response) {
-      try {
-         InputStream in = request.getInputStream();   
-         ByteArrayOutputStream baos = new ByteArrayOutputStream();  
-         byte[] content = new byte[1];
-         int bytesRead = -1;      
-         while( ( bytesRead = in.read( content ) ) != -1 ) {  
-            baos.write( content, 0, bytesRead );  
-         }
-         Clock clock = Clock.systemDefaultZone();
-         long milliSeconds=clock.millis();
-         OutputStream outputStream = new FileOutputStream(new File(String.valueOf(milliSeconds) + ".png"));
-         baos.writeTo(outputStream);
-         outputStream.close();
-         PrintWriter out = new PrintWriter(response.getOutputStream(), true);
-         File dir = new File(".");
-         String[] chld = dir.list();
-      	 for(int i = 0; i < chld.length; i++){
-            String fileName = chld[i];
-            out.println(fileName+"\n");
-            System.out.println(fileName);
-         }
-      } catch(Exception ex) {
-         System.err.println(ex);
-      }
-   }
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) {
+        try {
+            PrintWriter out = new PrintWriter(response.getOutputStream(), true);
+            // Build the HTML response
+            String htmlResponse = "<!DOCTYPE html>" +
+                    "<html>" +
+                    "<body>" +
+                    "<h2>Upload File</h2>" +
+                    "<form action=\"/upload\" method=\"post\" enctype=\"multipart/form-data\">" +
+                    "Caption: <input type=\"text\" name=\"caption\"/><br/><br/>" +
+                    "Date: <input type=\"date\" name=\"date\"/><br/><br/>" +
+                    "File: <input type=\"file\" name=\"fileName\"/><br/><br/>" +
+                    "<input type=\"submit\" value=\"Submit\"/>" +
+                    "</form>" +
+                    "</body>" +
+                    "</html>";
+
+            // Write the HTTP headers
+            out.println("HTTP/1.1 200 OK");
+            out.println("Content-Type: text/html");
+            out.println("Content-Length: " + htmlResponse.length());
+            out.println();
+            // Write the HTML content
+            out.print(htmlResponse);
+            out.flush();
+
+        } catch (Exception ex) {
+            System.err.println(ex);
+        }
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) {
+        try {
+            InputStream in = request.getInputStream();
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            byte[] content = new byte[1];
+            int bytesRead = -1;
+            while ((bytesRead = in.read(content)) != -1) {
+                baos.write(content, 0, bytesRead);
+            }
+            Clock clock = Clock.systemDefaultZone();
+            long milliSeconds = clock.millis();
+            OutputStream outputStream = new FileOutputStream(new File(String.valueOf(milliSeconds) + ".png"));
+            baos.writeTo(outputStream);
+            outputStream.close();
+
+            PrintWriter out = new PrintWriter(response.getOutputStream(), true);
+            File dir = new File(".");
+            String[] chld = dir.list();
+            for (String fileName : chld) {
+                out.println(fileName + "\n");
+                System.out.println(fileName);
+            }
+        } catch (Exception ex) {
+            System.err.println(ex);
+        }
+    }
 }
