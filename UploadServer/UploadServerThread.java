c973003e1b28201b4d1aed464e60c465d988807c
Tom He <the18@my.bcit.ca>
2024-10-11 10:35:03 -0700

    fixed bug where first line of heeader would not be parsed properly leading to problems with html form submission

--- UploadServer/UploadServerThread.java
+++ UploadServer/UploadServerThread.java
@@ -1,31 +1,5 @@
-// import java.net.*;
-// import java.io.*;
-// import java.time.Clock;
-// public class UploadServerThread extends Thread {
-//    private Socket socket = null;
-//    public UploadServerThread(Socket socket) {
-//       super("DirServerThread");
-//       this.socket = socket;
-//    }
-//    public void run() {
-//       try {
-//          InputStream in = socket.getInputStream(); 
-//          HttpServletRequest req = new HttpServletRequest(in);  
-//          OutputStream baos = new ByteArrayOutputStream(); 
-//          HttpServletResponse res = new HttpServletResponse(baos); 
-//          HttpServlet httpServlet = new UploadServlet();
-//          httpServlet.doPost(req, res);
-//          OutputStream out = socket.getOutputStream(); 
-//          out.write(((ByteArrayOutputStream) baos).toByteArray());
-//          socket.close();
-//       } catch (Exception e) { e.printStackTrace(); }
-//    }
-// }
-
 import java.net.*;
 import java.io.*;
-import java.util.StringTokenizer;
-
 
 public class UploadServerThread extends Thread {
     private Socket socket = null;
@@ -36,60 +10,93 @@ public class UploadServerThread extends Thread {
     }
 
     public void run() {
-    
+
         try {
             OutputStream out = socket.getOutputStream();
-            // Use BufferedInputStream to read the request
             BufferedInputStream in = new BufferedInputStream(socket.getInputStream());
             BufferedReader reader = new BufferedReader(new InputStreamReader(in));
 
             // Read the first line of the request
-            String requestLine = reader.readLine();
+            String requestLine = "";
             if (requestLine != null) {
-                String[] requestParts = requestLine.split(" ");
-                String method = requestParts[0];
-                String uri = requestParts[1];
-                String boundary = "";
-                String temp;
-                if ("POST".equalsIgnoreCase(method)) {
-                    while ((temp = reader.readLine()) != "\r\n\r\n") {
-                        if (temp.contains("boundary=")) {
-                            boundary = temp.substring(temp.indexOf("boundary=") + 9);
-                            break;
-                        } 
+                String method = "";
+                while ((requestLine = reader.readLine()) != null) {
+                    if (requestLine.contains("GET") || requestLine.contains("get")) {
+                        method = "GET";
+                        break;
+                    } else if (requestLine.contains("POST")|| requestLine.contains("post")) {
+                        method = "POST";
+                        break;
                     }
                 }
-                HttpServletRequest req = new HttpServletRequest(in);
-                req.setBoundary(boundary);
-                OutputStream baos = new ByteArrayOutputStream();
-                HttpServletResponse res = new HttpServletResponse(baos);
-                HttpServlet httpServlet = new UploadServlet();
+
+                String boundary = "";
+                String temp;
+                int contentLength = -1;
+
+                // Read headers
+                while (!(temp = reader.readLine()).isEmpty()) {
+                    if (temp.contains("boundary=")) {
+                        boundary = temp.substring(temp.indexOf("boundary=") + 9);
+                    } else if (temp.toLowerCase().startsWith("content-length:")) {
+                        contentLength = Integer.parseInt(temp.substring(16).trim());
+                        System.out.println("Content-Length: " + contentLength);
+                    }
+                }
 
                 // Check the HTTP method
-                if ("GET".equalsIgnoreCase(method)) {
-                    // Serve the HTML file
-                File htmlFile = new File("Form.html");
-                FileInputStream fileInputStream = new FileInputStream(htmlFile);
-                String content = "HTTP/1.1 200 OK\r\n" +
-                                 "Content-Type: text/html\r\n" +
-                                 "Content-Length: " + htmlFile.length() + "\r\n" +
-                                 "\r\n";
-                out.write(content.getBytes());
-                byte[] buffer = new byte[4096];
-                int bytesRead;
-                while ((bytesRead = fileInputStream.read(buffer)) != -1) {
-                    out.write(buffer, 0, bytesRead);
-                }
-                fileInputStream.close();
-                } else if ("POST".equalsIgnoreCase(method)) {
-                    httpServlet.doPost(req, res);
-                } else {
-                    // Handle unsupported methods
-                    res.getOutputStream().write("HTTP/1.1 405 Method Not Allowed\r\n\r\n".getBytes());
-                }
+                if ("POST".equalsIgnoreCase(method)) {
+                    // Only read as much as Content-Length specifies
+                    byte[] requestBody = new byte[contentLength];
+                    int totalBytesRead = 0;
+                    int bytesRead;
+                    while (totalBytesRead < contentLength && (bytesRead = in.read(requestBody, totalBytesRead,
+                            contentLength - totalBytesRead)) != -1) {
+                        totalBytesRead += bytesRead;
+                        System.out.println("Bytes read: " + bytesRead + ", Total so far: " + totalBytesRead);
+                    }
 
-                // Send the response back to the client
-                out.write(((ByteArrayOutputStream) baos).toByteArray());
+                    if (totalBytesRead < contentLength) {
+                        System.out.println("Error: Not enough data received. Expected " + contentLength + " but got " + totalBytesRead);
+                        out.write("HTTP/1.1 400 Bad Request\r\n\r\n".getBytes());
+                        socket.close();
+                        return;
+                    }
+
+                    System.out.println("Body fully received. Total bytes: " + totalBytesRead);
+
+                    // Now wrap it into a new InputStream that we can pass to HttpServletRequest
+                    ByteArrayInputStream cachedInputStream = new ByteArrayInputStream(requestBody);
+
+                    HttpServletRequest req = new HttpServletRequest(cachedInputStream);
+                    req.setBoundary(boundary);
+                    OutputStream baos = new ByteArrayOutputStream();
+                    HttpServletResponse res = new HttpServletResponse(baos);
+                    HttpServlet httpServlet = new UploadServlet();
+
+                    // Call the doPost method of the servlet
+                    httpServlet.doPost(req, res);
+
+                    // Send the response back to the client
+                    out.write(((ByteArrayOutputStream) baos).toByteArray());
+                } else if ("GET".equalsIgnoreCase(method)) {
+                    System.out.println("GET request received");
+                    // Serve the HTML file for GET request
+                    int bytesRead;
+                    File htmlFile = new File("Form.html");
+                    FileInputStream fileInputStream = new FileInputStream(htmlFile);
+                    String content = "HTTP/1.1 200 OK\r\n" +
+                            "Content-Type: text/html\r\n" +
+                            "Content-Length: " + htmlFile.length() + "\r\n" +
+                            "\r\n";
+                    out.write(content.getBytes());
+                    byte[] buffer = new byte[4096];
+                    while ((bytesRead = fileInputStream.read(buffer)) != -1) {
+                        out.write(buffer, 0, bytesRead);
+                    }
+                    fileInputStream.close();
+                }
+                // Flush and close the socket
                 out.flush();
                 socket.close();
             }
